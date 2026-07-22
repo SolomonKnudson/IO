40cc43fe8bcc608654e611ba71a34fe70d77d257
Solomon <solomonknudson@gmail.com>
2026-07-22 12:29:34 -0400

    named stream function now use internal::read<>()/write<>()

--- include/io/iostream.hpp
+++ include/io/iostream.hpp
@@ -1,6 +1,6 @@
 #ifndef IO_STREAM_HPP
 #define IO_STREAM_HPP
-#include <io/internal/impl/iostream.hpp>
+#include <io/internal/internal.hpp>
 
 namespace io
 {
@@ -10,63 +10,61 @@ namespace io
     decltype(auto)
     cin(LValue&& l_value)
     {
-      Operator::operation<internal::impl::cin>(std::forward<LValue>(l_value));
-    };
+      return internal::read<internal::impl::cin>(std::forward<LValue>(l_value));
+    }
 
     template <typename LValue>
     decltype(auto)
     wcin(LValue&& l_value)
     {
-      Operator::operation<internal::impl::wcin>(std::forward<LValue>(l_value));
-    };
+      return internal::read<internal::impl::wcin>(
+          std::forward<LValue>(l_value));
+    }
 
     template <typename... Args>
     decltype(auto)
     cout(Args&&... args)
     {
-      return Operator::operation<internal::impl::cout>(
-          std::forward<Args>(args)...);
-    };
+      return internal::write<internal::impl::cout>(std::forward<Args>(args)...);
+    }
 
     template <typename... Args>
     decltype(auto)
     wcout(Args&&... args)
     {
-      return Operator::operation<internal::impl::wcout>(
+      return internal::write<internal::impl::wcout>(
           std::forward<Args>(args)...);
-    };
+    }
 
     template <typename... Args>
     decltype(auto)
     cerr(Args&&... args)
     {
-      return Operator::operation<internal::impl::cerr>(
-          std::forward<Args>(args)...);
-    };
+      return internal::write<internal::impl::cerr>(std::forward<Args>(args)...);
+    }
 
     template <typename... Args>
     decltype(auto)
     wcerr(Args&&... args)
     {
-      return Operator::operation<internal::impl::wcerr>(
+      return internal::write<internal::impl::wcerr>(
           std::forward<Args>(args)...);
-    };
+    }
 
     template <typename... Args>
     decltype(auto)
     clog(Args&&... args)
     {
-      return Operator::operation<internal::impl::clog>(
-          std::forward<Args>(args)...);
-    };
+      return internal::write<internal::impl::clog>(std::forward<Args>(args)...);
+    }
 
     template <typename... Args>
     decltype(auto)
     wclog(Args&&... args)
     {
-      return Operator::operation<internal::impl::clog>(
+      return internal::write<internal::impl::wclog>(
           std::forward<Args>(args)...);
-    };
+    }
   } // namespace stream
 }; // namespace io
 #endif // IO_STREAM_HPP
