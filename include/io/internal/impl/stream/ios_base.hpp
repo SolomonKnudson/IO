01c3ec74afefb7c8eb563123901a83f9a03d8660
Solomon <solomonknudson@gmail.com>
2026-07-24 00:42:12 -0400

    start implementing shared stream method wrappers

--- include/io/internal/impl/stream/ios_base.hpp
+++ include/io/internal/impl/stream/ios_base.hpp
@@ -31,4 +31,158 @@ template <> struct Operator::Impl<io::internal::impl::ios_base>
     return out_stream;
   }
 };
+
+// Formatting
+// TODO:
+// copyfmt
+// fill
+
+template <> struct Operator::Impl<io::internal::impl::flags>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke()
+  {
+    return StreamTag::stream().flags();
+  }
+
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const std::ios_base::fmtflags flags)
+  {
+    return StreamTag::stream().flags(flags);
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::setf>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const std::ios_base::fmtflags flags)
+  {
+    return StreamTag::stream().setf(flags);
+  }
+
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const std::ios_base::fmtflags flags,
+         const std::ios_base::fmtflags mask)
+  {
+    return StreamTag::stream().setf(flags, mask);
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::unsetf>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const std::ios_base::fmtflags flags)
+  {
+    return StreamTag::stream().unsetf(flags);
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::precision>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke()
+  {
+    return StreamTag::stream().precision();
+  }
+
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const std::streamsize new_precision)
+  {
+    return StreamTag::stream().precision(new_precision);
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::width>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke()
+  {
+    return StreamTag::stream().width();
+  }
+
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const std::streamsize new_width)
+  {
+    return StreamTag::stream().width(new_width);
+  }
+};
+
+// TODO:
+// State
+// good
+// eof
+// fail
+// bad
+// rdstate
+// setstate
+// clear
+
+// Locales
+template <> struct Operator::Impl<io::internal::impl::imbue>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const std::locale& loc)
+  {
+    return StreamTag::stream().imbue(loc);
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::getloc>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke()
+  {
+    return StreamTag::stream().getloc();
+  }
+};
+
+// Internal extensible array
+template <> struct Operator::Impl<io::internal::impl::iword>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const int32_t index)
+  {
+    return StreamTag::stream().iword(index);
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::pword>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(const int32_t index)
+  {
+    return StreamTag::stream().pword(index);
+  }
+};
+
+// Miscellaneous
+template <> struct Operator::Impl<io::internal::impl::register_callback>
+{
+  template <typename StreamTag>
+  static decltype(auto)
+  invoke(std::ios_base::event_callback function, const int32_t index)
+  {
+    return StreamTag::stream().register_callback(function, index);
+  }
+};
+
+// TODO:
+// execptions
+// rdbuf
+// tie
+// narrow
+// widen
 #endif // IO_INTERNAL_IOS_BASE_IMPL_HPP
--- include/io/internal/impl/tags.hpp
+++ include/io/internal/impl/tags.hpp
@@ -8,18 +8,61 @@ namespace io::internal::impl
   // IOstream impls follow STL convention
   // Insertion: >>. 'Outsertion': <<
   OPERATOR_CREATE_IMPL_TAG(ios_base);
+
+  // Shared methods
+  // Formatting
+  OPERATOR_CREATE_IMPL_TAG(flags);
+  OPERATOR_CREATE_IMPL_TAG(setf);
+
+  OPERATOR_CREATE_IMPL_TAG(unsetf);
+  OPERATOR_CREATE_IMPL_TAG(precision);
+
+  OPERATOR_CREATE_IMPL_TAG(width);
+
+  // Locales
+  OPERATOR_CREATE_IMPL_TAG(imbue);
+  OPERATOR_CREATE_IMPL_TAG(getloc);
+
+  // Internal extensible array
+  OPERATOR_CREATE_IMPL_TAG(iword);
+  OPERATOR_CREATE_IMPL_TAG(pword);
+
+  // Miscellaneous
+  OPERATOR_CREATE_IMPL_TAG(register_callback);
+
+  // Unformatted input
+  // IStream methods
   OPERATOR_CREATE_IMPL_TAG(get);
-
   OPERATOR_CREATE_IMPL_TAG(peek);
+
   OPERATOR_CREATE_IMPL_TAG(unget);
-
   OPERATOR_CREATE_IMPL_TAG(putback);
+
   OPERATOR_CREATE_IMPL_TAG(getline);
-
   OPERATOR_CREATE_IMPL_TAG(ignore);
-  OPERATOR_CREATE_IMPL_TAG(read);
 
+  OPERATOR_CREATE_IMPL_TAG(read);
   OPERATOR_CREATE_IMPL_TAG(readsome);
+
   OPERATOR_CREATE_IMPL_TAG(gcount);
+
+  // OStream methods
+  OPERATOR_CREATE_IMPL_TAG(put);
+  OPERATOR_CREATE_IMPL_TAG(write);
+
+  // Positioning
+  // IStream methods
+  OPERATOR_CREATE_IMPL_TAG(tellg);
+  OPERATOR_CREATE_IMPL_TAG(seekg);
+
+  // OStream methods
+  OPERATOR_CREATE_IMPL_TAG(tellp);
+  OPERATOR_CREATE_IMPL_TAG(seekp);
+
+  // Miscellaneous
+  OPERATOR_CREATE_IMPL_TAG(sync);
+
+  // OStream methods
+  OPERATOR_CREATE_IMPL_TAG(flush);
 } // namespace io::internal::impl
 #endif // IO_INTERNAL_IMPL_TAGS_HPP
