c8b662cc7228649fca2b8b5e5cae01780beca3fd
Solomon <solomonknudson@gmail.com>
2026-07-24 00:41:43 -0400

    implement ostream method wrappers

--- include/io/internal/impl/stream/ostream.hpp
+++ include/io/internal/impl/stream/ostream.hpp
@@ -68,4 +68,66 @@ template <> struct Operator::Impl<io::tags::stream::wclog>
         io::tags::stream::wclog::stream(), std::forward<Args>(args)...);
   }
 };
+
+// Unformatted input
+template <> struct Operator::Impl<io::internal::impl::put>
+{
+  template <typename OStreamTag>
+  static decltype(auto)
+  invoke(const typename OStreamTag::stream_type::char_type ch)
+  {
+    return OStreamTag::stream().put(ch);
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::write>
+{
+  template <typename OStreamTag>
+  static decltype(auto)
+  invoke(const typename OStreamTag::stream_type::char_type* ch,
+         const std::streamsize count)
+  {
+    return OStreamTag::stream().write(ch, count);
+  }
+};
+
+// Positioning
+template <> struct Operator::Impl<io::internal::impl::tellp>
+{
+  template <typename OStreamTag>
+  static typename OStreamTag::stream_type::pos_type
+  invoke()
+  {
+    return OStreamTag::stream().tellp();
+  }
+};
+
+template <> struct Operator::Impl<io::internal::impl::seekp>
+{
+  template <typename OStreamTag>
+  static decltype(auto)
+  invoke(const typename OStreamTag::stream_type::pos_type pos)
+  {
+    return OStreamTag::stream().seekp(pos);
+  }
+
+  template <typename OStreamTag>
+  static decltype(auto)
+  invoke(const typename OStreamTag::stream_type::off_type off,
+         std::ios_base::seekdir& dir)
+  {
+    return OStreamTag::stream().seekp(off, dir);
+  }
+};
+
+// Miscellaneous
+template <> struct Operator::Impl<io::internal::impl::flush>
+{
+  template <typename OStreamTag>
+  static decltype(auto)
+  invoke()
+  {
+    return OStreamTag::stream().flush();
+  }
+};
 #endif // IO_INTERNAL_OSTREAM_IMPL_HPP
--- include/io/ostream.hpp
+++ include/io/ostream.hpp
@@ -54,6 +54,65 @@ namespace io
       return Operator::operation<tags::stream::wclog>(
           std::forward<Args>(args)...);
     }
+    // Unformatted input
+    template <typename OStreamTag = tags::stream::cout>
+    decltype(auto)
+    put(const typename OStreamTag::stream_type::char_type ch)
+    {
+      static_assert(OStreamTag::is_writable,
+                    "io::stream::put(): stream must "
+                    "be writable!");
+
+      return Operator::operation<internal::impl::put, OStreamTag>(ch);
+    }
+
+    template <typename OStreamTag = tags::stream::cout>
+    decltype(auto)
+    write(const typename OStreamTag::stream_type::char_type* ch,
+          const std::streamsize count)
+    {
+      static_assert(OStreamTag::is_writable,
+                    "io::stream::write(): stream must "
+                    "be writable!");
+
+      return Operator::operation<internal::impl::write, OStreamTag>(ch, count);
+    }
+
+    // Positioning
+    template <typename OStreamTag = tags::stream::cout>
+    decltype(auto)
+    tellp()
+    {
+      static_assert(OStreamTag::is_writable,
+                    "io::stream::tellg(): stream must "
+                    "be writable!");
+
+      return Operator::operation<internal::impl::tellp, OStreamTag>();
+    }
+
+    template <typename OStreamTag = tags::stream::cout, typename... Args>
+    decltype(auto)
+    seekp(Args&&... args)
+    {
+      static_assert(OStreamTag::is_writable,
+                    "io::stream::seekg(): stream must "
+                    "be wriable!");
+
+      return Operator::operation<internal::impl::seekp, OStreamTag>(
+          std::forward<Args>(args)...);
+    }
+
+    // Miscellaneous
+    template <typename OStreamTag = tags::stream::cout>
+    decltype(auto)
+    flush()
+    {
+      static_assert(OStreamTag::is_writable,
+                    "io::stream::flush(): stream must "
+                    "be wriable!");
+
+      return Operator::operation<internal::impl::flush, OStreamTag>();
+    }
   } // namespace stream
 }; // namespace io
 #endif // IO_OSTREAM_HPP
